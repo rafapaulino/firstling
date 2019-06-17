bb0441ac632cd71064a75e72a86b2ade616b04a7
Rafael Brigagao Paulino <rafael.paulino@FIESP.COM.BR>
2019-06-17 10:37:03 -0300

    opções de customização do tema

--- wp-content/themes/firstling/custom/theme-custom-options-kirki.php
+++ wp-content/themes/firstling/custom/theme-custom-options-kirki.php
@@ -52,21 +52,25 @@ add_action( 'customize_register', 'firstling_customize_register');
 
 function firstling_kirki_fields( $fields ) {
 
+    $img_dir = get_template_directory_uri() . '/assets/images/';
 
     //https://gist.github.com/aristath/d778d2592a4a6ce8f640
     $fields[] = array(
 		'type'        => 'radio-image',
-		'setting'     => 'radio_image_demo',
-		'label'       => __( 'This is the label', 'kirki' ),
-		'description' => __( 'This is the control description', 'kirki' ),
-		'help'        => __( 'This is some extra help. You can use this to add some additional instructions for users. The main description should go in the "description" of the field, this is only to be used for help tips.', 'kirki' ),
+		'setting'     => 'classe_tema',
+		'label'       => __( 'Esquema de Cores', 'kirki' ),
+		'description' => __( 'Escolha qual esquema de cores você deseja para o seu tema.', 'kirki' ),
+		'help'        => __( 'Escolha qual esquema de cores você deseja para o seu tema.', 'kirki' ),
 		'section'     => 'firstling_configuracoes_principais',
-		'default'     => 'option-1',
+		'default'     => 'default',
 		'priority'    => 10,
 		'choices'     => array(
-			'option-1' => admin_url() . '/images/align-left-2x.png',
-			'option-2' => admin_url() . '/images/align-center-2x.png',
-			'option-3' => admin_url() . '/images/align-right-2x.png',
+			'default' => $img_dir . 'default.png',
+			'green'   => $img_dir . 'green.png',
+            'pink'    => $img_dir . 'pink.png',
+            'purple'  => $img_dir . 'purple.png',
+            'red'     => $img_dir . 'red.png',
+            'warning' => $img_dir . 'warning.png',
 		),
     );
     
@@ -96,3 +100,32 @@ function firstling_kirki_fields( $fields ) {
 
 }
 add_filter( 'kirki/fields', 'firstling_kirki_fields' );
+
+
+//adiciona a classe no body
+function firstling_custom_body_class($classes) 
+{
+    $classe_tema = Kirki::get_option( 'classe_tema' );
+    $sobrescrever_cores = Kirki::get_option( 'sobrescrever_cores' );
+
+    if(trim($classe_tema) !== "" && $classe_tema !== "default" && $sobrescrever_cores === false)
+    $classes[] = $classe_tema;
+    
+    if($sobrescrever_cores)
+    $classes[] = 'custom-color';
+
+    return $classes;
+}
+//Now add test class to the filter
+add_filter('body_class','firstling_custom_body_class');
+
+//adiciona a cor personalizada no header
+function firstling_hook_css() 
+{
+    $sobrescrever_cores = Kirki::get_option( 'sobrescrever_cores' );
+    $cor_de_fundo = Kirki::get_option( 'cor_de_fundo' );
+?><style>
+body.custom-color .navContainer{background-color:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color #footer{border-bottom-color:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color .topBottom-leftRightCorner .ui-border-element::before,body.custom-color .topBottom-leftRightCorner .ui-border-element::after,body.custom-color .topBottom-leftRightCorner::before,body.custom-color .topBottom-leftRightCorner::after{border-color:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color .categories-list li a:hover{color:<?php esc_attr_e($cor_de_fundo); ?> !important}body.custom-color h2 a:hover,body.custom-color h3 a:hover,body.custom-color h4 a:hover,body.custom-color h5 a:hover,body.custom-color h6 a:hover{color:<?php esc_attr_e($cor_de_fundo); ?> !important}body.custom-color .entry-content h1,body.custom-color .entry-content h2,body.custom-color .entry-content h3,body.custom-color .entry-content h4,body.custom-color .entry-content h5,body.custom-color .entry-content h6{color:<?php esc_attr_e($cor_de_fundo); ?> !important}body.custom-color .plyr--full-ui input[type="range"]{color:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color .plyr--audio .plyr__control.plyr__tab-focus,body.custom-color .plyr--audio .plyr__control:hover,body.custom-color .plyr--audio .plyr__control[aria-expanded="true"]{background:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color .plyr--audio .plyr__control.plyr__tab-focus,body.custom-color .plyr--audio .plyr__control:hover,body.custom-color .plyr--audio .plyr__control[aria-expanded="true"]{background:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color .author.vcard a{color:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color .comments-link a{color:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color .media-heading a{color:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color .comment-metadata a{color:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color #comments .btn-comment{background:<?php esc_attr_e($cor_de_fundo); ?>}body.custom-color .pagination-wrap .page-numbers li span{background:<?php esc_attr_e($cor_de_fundo); ?>;border:1px solid <?php esc_attr_e($cor_de_fundo); ?>}
+</style><?php
+}
+add_action('wp_head', 'firstling_hook_css');
